b2ccc9eee1732fc5b3a131d23f1c7d1ba69c01c9
Pablo Gastelum <gastelum.pablo@gmail.com>
2025-07-17 02:06:49 -0600

    test with os temp dir

--- src/repositories/orders.repository.ts
+++ src/repositories/orders.repository.ts
@@ -1,13 +1,19 @@
 import path from "path";
 import fs from "fs";
+import os from "os";
 import { Order } from "../models/order";
 import { handleError, writeFile } from "../utils";
 
-const ORDERS_PATH = path.join(__dirname, "../data/orders.json");
+const TEMP_ORDERS_PATH = path.join(os.tmpdir(), "orders.json");
 
 const getOrders = (): Order[] => {
   try {
-    const ordersFile = fs.readFileSync(ORDERS_PATH, "utf-8");
+    if (!fs.existsSync(TEMP_ORDERS_PATH)) {
+      // Si no existe, creamos archivo con un array vacío
+      fs.writeFileSync(TEMP_ORDERS_PATH, "[]", "utf-8");
+    }
+
+    const ordersFile = fs.readFileSync(TEMP_ORDERS_PATH, "utf-8");
     return JSON.parse(ordersFile) as Order[];
   } catch (error) {
     throw handleError(error);
@@ -18,12 +24,9 @@ const addOrder = (newOrder: Order): Order => {
   try {
     newOrder.id = crypto.randomUUID();
 
-    fs.readFile(ORDERS_PATH, "utf-8", (error, ordersFile) => {
-      if (error) handleError(error);
-      const ordersList = JSON.parse(ordersFile);
-      ordersList.push(newOrder);
-      writeFile(ORDERS_PATH, ordersList);
-    });
+    const ordersList = getOrders(); // Usamos la función anterior
+    ordersList.push(newOrder);
+    fs.writeFileSync(TEMP_ORDERS_PATH, JSON.stringify(ordersList, null, 2), "utf-8");
 
     return newOrder;
   } catch (error) {
